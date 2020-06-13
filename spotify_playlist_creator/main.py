25fb093a972aacaae102675f6e0c269f6fefdc32
lafftar <tibabalase@gmail.com>
2020-06-13 05:04:04 -0400

    Basic Spotify playlist and first steps of building a front end for the app.

---
+++ spotify_playlist_creator/main.py
@@ -0,0 +1,78 @@
+from spotify_playlist_creator import spotify_api_interface
+from spotify_playlist_creator import sorter
+from time import time
+
+def create_top_50(artist_name):
+    t1 = time()
+    spotify_api_interface.grab_all_songs(artist_name)
+    data_frame = sorter.grab_data_from_table(artist_name)
+    sorter.sort_and_dump_data_to_db(data_frame,artist_name)
+    spotify_api_interface.add_tracks_to_playlist(artist_name)
+    t2 = time()
+    print(f'{artist_name} took {t2-t1} seconds')
+
+
+
+if __name__ == '__main__':
+    list_of_artist_names = ['Taylor Swift',
+                            'Ariana Grande',
+                            'Shawn Mendes',
+                            'Maroon 5',
+                            'Adele',
+                            'Justin Bieber',
+                            'Ed Sheeran',
+                            'Justin Timberlake',
+                            'Charlie Puth',
+                            'John Mayer',
+                            'Lorde',
+                            'Fifth Harmony',
+                            'Lana Del Rey',
+                            'James Arthur',
+                            'Zara Larsson',
+                            'Pentatonix',
+                            'Kendrick Lamar',
+                            'Post Malone',
+                            'Drake',
+                            'Kanye West',
+                            'Eminem',
+                            'Future',
+                            '50 Cent',
+                            'Lil Wayne',
+                            'Lil Baby',
+                            'DaBaby',
+                            'Macklemore',
+                            'Jay-Z',
+                            'Bruno Mars',
+                            'Beyoncé',
+                            'Enrique Iglesias',
+                            'Stevie Wonder',
+                            'John Legend',
+                            'Alicia Keys',
+                            'Usher',
+                            'Rihanna',
+                            'Kygo',
+                            'The Chainsmokers',
+                            'Avicii',
+                            'Marshmello',
+                            'Calvin Harris',
+                            'Martin Garrix',
+                            'Coldplay',
+                            'Elton John',
+                            'OneRepublic',
+                            'The Script',
+                            'Jason Mraz',
+                            'Frank Sinatra',
+                            'Michael Bublé',
+                            'Norah Jones']
+
+    t1 = time()
+    for artist in list_of_artist_names:
+        create_top_50(artist)
+    t2 = time()
+
+    print('-----------------------------------------------------------------')
+    print(f'Only took {t2-t1} seconds!')
+    print('-----------------------------------------------------------------')
+
+
+
