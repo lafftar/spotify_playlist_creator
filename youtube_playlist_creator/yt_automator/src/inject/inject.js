0fe8970d64ff1441af09844d091e5a1d89e4bd16
lafftar <tibabalase@gmail.com>
2020-08-29 12:42:21 -0400

    Update inject.js

--- youtube_playlist_creator/yt_automator/src/inject/inject.js
+++ youtube_playlist_creator/yt_automator/src/inject/inject.js
@@ -5,7 +5,7 @@ chrome.extension.sendMessage({}, function (response) {
 
             // ----------------------------------------------------------
             // This part of the script triggers when page is done loading
-            function start() {
+            async function start() {
                 var simulateMouseEvent = function (element, eventName, coordX, coordY) {
                     element.dispatchEvent(new MouseEvent(eventName, {
                         view: window,
@@ -18,7 +18,7 @@ chrome.extension.sendMessage({}, function (response) {
                 };
                 console.log("Hello. This message was sent from scripts/inject.js");
                 document.querySelector('ytd-menu-renderer.ytd-playlist-sidebar-primary-info-renderer > yt-icon-button:nth-child(2)').click();
-                this.sleep(500);
+                await this.sleep(500);
                 document.querySelector('#items > ytd-menu-service-item-renderer:nth-child(1) > paper-item > yt-formatted-string').click();
                 var iframe = document.getElementsByTagName('iframe')[1];
                 var theButton = document.querySelector("#\\:6 > div");
